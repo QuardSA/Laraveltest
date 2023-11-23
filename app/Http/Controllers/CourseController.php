7e3bfa89208ce31cf13964d097759fd7e82033b6
QuardSA <smirnov_aleksey04@inbox.ru>
2023-11-23 19:00:38 +0500

    valid repair

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Course;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
 
 class CourseController extends Controller
@@ -11,32 +12,29 @@ class CourseController extends Controller
     public function index()
     {
         $courses = Course::paginate(2);
-        return view('index',
+        return view(
+            'index',
             compact("courses")
         );
     }
 
     public function create(Request $request)
     {
-        $course_info = $request->all();
-        $request->validate(
-            [
-                'title' => 'required|title',
-                'description' => 'required|description',
-                'duration' => 'required|duration',
-                'cost' => 'required|cost',
-                'category_id' => 'required|category_id',
-            ],
-            [
-                'title.required' => 'Поле title не заполнено!',
-                'title.title' => 'Укажите действительный адрес почты',
-                'description.required' => 'Поле description не заполнено!',
-                'duration.required' => 'Поле duration не заполнено!',
-                'cost.required' => 'Поле cost не заполнено!',
-                'category_id.required' => 'Поле Category не заполнено!',
-                'img.required' => 'Поле img не заполнено!',
-            ],
-        );
+        $request->validate([
+            "title" => "required|string",
+            "description" => "required|string",
+            "duration" => "required|integer",
+            "cost" => "required|integer",
+            "category_id" => "required",
+            "img" => "required",
+        ], [
+            "title.required" => "Поле title не заполнено!",
+            "description.required" => "Поле description не заполнено!",
+            "duration.required" => "Поле duration не заполнено!",
+            "cost.required" => "Поле cost не заполнено!",
+            "category_id.required" => "Поле Category не заполнено!",
+            "img.required" => "Поле img не заполнено!",
+        ]);
 
         $course_info = $request->all();
 
