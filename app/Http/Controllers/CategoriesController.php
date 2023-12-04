b43bfdccf37bc56f4717416458c86962d3dab4f9
QuardSA <smirnov_aleksey04@inbox.ru>
2023-12-04 17:47:44 +0500

    Fix filter

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,16 +8,9 @@ use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-    public function show()
-    {
-        $categories = Category::all();
-        return view("index", ['categories'=>$categories]);
-    }
     public function categories($id)
     {
         $categories_find = Category::find($id)->categories;
-
-        // dd ($categories_find);
         return view('category', ['categories'=>$categories_find]);
     }
 }
